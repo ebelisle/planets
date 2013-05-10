0c3a9119699081be22f4776a6ec90fb258ee1ab8
Ed Belisle <ed@bellobjects.com>
2013-05-10 13:54:09 -0600

    Added expectedExceptions and dependsOnMethods to tests

--- src/test/java/com/bellobjects/planets/SampleTest.java
+++ src/test/java/com/bellobjects/planets/SampleTest.java
@@ -13,54 +13,61 @@ public class SampleTest {
         org.testng.AssertJUnit.assertTrue("Not True", true);
     }
 
+    @Test(expectedExceptions = java.lang.IllegalArgumentException.class)
     public void badOrgSpringframeworkUtilAssertTest() {
         org.springframework.util.Assert.isTrue(false, "Not True");
     }
 
+    @Test(expectedExceptions = java.lang.AssertionError.class)
     public void badOrgTestngAssertTest() {
         org.testng.Assert.assertTrue(false, "Not True");
     }
 
+    @Test(expectedExceptions = junit.framework.AssertionFailedError.class)
     public void badJunitFrameworkAssertTest() {
         junit.framework.Assert.assertTrue(false);
     }
 
+    @Test(expectedExceptions = java.lang.AssertionError.class)
     public void badOrgJAssertTest() {
         org.junit.Assert.assertTrue("Not True", false);
     }
 
+    @Test(expectedExceptions = java.lang.AssertionError.class)
     public void badOrgTestngAssertJUnitTest() {
         org.testng.AssertJUnit.assertTrue("Not True", false);
     }
+    
+    private int state = 0;
 
-    @Test(dependsOnMethods="badOrgSpringframeworkUtilAssertTest")
-    public void dependantOrgSpringframeworkUtilAssertTest() {
+    public void state0() {
+        org.testng.Assert.assertEquals(0, state++);
     }
 
-    @Test(dependsOnMethods="badOrgTestngAssertTest")
-    public void dependantOrgTestngAssertTest() {
+    @Test(dependsOnMethods = "state0")
+    public void state1() {
     }
 
-    @Test(dependsOnMethods="badJunitFrameworkAssertTest")
+    @Test(dependsOnMethods = "badJunitFrameworkAssertTest")
     public void dependantJunitFrameworkAssertTest() {
     }
 
-    @Test(dependsOnMethods="badOrgJAssertTest")
+    @Test(dependsOnMethods = "badOrgJAssertTest")
     public void dependantOrgJAssertTest() {
     }
 
-    @Test(dependsOnMethods="badOrgTestngAssertJUnitTest")
+    @Test(dependsOnMethods = "badOrgTestngAssertJUnitTest")
     public void dependantOrgTestngAssertJUnitTest() {
     }
-    
+
     public void runtimeFailingTest() {
         throw new RuntimeException("Intentional Runtime Exception");
     }
-    
+
     public boolean ignoredTest() {
-        return true;
+        throw new RuntimeException("methods with return values are not tests");
     }
-    
+
     public void includedTest() {
         
     }
